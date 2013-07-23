ef1d5dc6c5785d922db00c3874b2445c29da0c03
squallsama <squall.sama@gmail.com>
2013-07-23 18:26:07 +0400

    fix postgresql bug in model.py

--- server/mirrormanager/model.py
+++ server/mirrormanager/model.py
@@ -235,7 +235,7 @@ class Host(SQLObject):
     internet2_clients = BoolCol(default=False)
     asn = IntCol(default=None)
     asn_clients = BoolCol(default=True)
-    max_connections = IntCol(default=1, notNone=True, unsigned=True)
+    max_connections = IntCol(default=1, notNone=True, unsigned=False)
     countries_allowed = MultipleJoin('HostCountryAllowed')
     netblocks = MultipleJoin('HostNetblock', orderBy='netblock')
     acl_ips = MultipleJoin('HostAclIp', orderBy='ip')
